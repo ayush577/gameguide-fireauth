cc4d80bacfacc03f58e88448d448ad23bb413732
ayush577 <agandhi62@gmail.com>
2020-03-13 19:39:10 +0530

    The Net Ninija lesson-11

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,20 @@
+// Auth Status
+auth.onAuthStateChanged(user => {
+  if (user) {
+    // GET DATA
+    db.collection("guides")
+      .get()
+      .then(snapshot => {
+        // console.log(snapshot.docs);
+        setupGuides(snapshot.docs);
+        setupUI(user);
+      });
+  } else {
+    setupGuides([]);
+    setupUI();
+  }
+});
+
 // SignUp
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", e => {
@@ -10,9 +27,32 @@ signupForm.addEventListener("submit", e => {
 
   // Sign Up User
   auth.createUserWithEmailAndPassword(email, password).then(creb => {
-    console.log(creb.user);
     const modal = document.querySelector("#modal-signup");
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   });
 });
+
+//Log Out
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", e => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+//Log In
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  //Get the User Info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
+
